3cd1e0a95bfcb813467f5d7f9947ae3da313a273
jnskkmhr <junjun.k2012@gmail.com>
2021-12-22 14:24:16 +0900

    fixed dataset path

--- sin.sh
+++ sin.sh
@@ -3,4 +3,4 @@
 conda init && \
 source ~/.bashrc
 source /opt/anaconda3/bin/activate salsanext
-source ~/SalsaNext/train.sh -d /groups/gcd50654/tier4/dataset/semantickitti -a ~/SalsaNext/salsanext.yml -l ~/SalsaNext/log_pi -c 0,1,2,3 -t /groups/gcd50654/tier4/dataset/vls
+source ~/SalsaNext/train.sh -d /groups/gcd50654/tier4/dataset/semantickitti/dataset -a ~/SalsaNext/salsanext.yml -l ~/SalsaNext/log_pi -c 0,1,2,3 -t /groups/gcd50654/tier4/dataset/vls
