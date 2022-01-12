7c5c647bd6c9ba80e05fb3cb95203814fa46146e
jnskkmhr <junjun.k2012@gmail.com>
2022-01-12 16:17:20 +0900

    fix

--- train/tasks/semantic/modules/trainer.py
+++ train/tasks/semantic/modules/trainer.py
@@ -139,7 +139,7 @@ class Trainer():
                                                max_points=self.ARCH["dataset"]["max_points"],
                                                batch_size=self.ARCH["train"]["batch_size_test"],
                                                workers=self.ARCH["train"]["workers"],
-                                               gt=True,
+                                               gt=False,
                                                shuffle_train=False)
 
         # concat trainset and testset and transform it to dataloader 
