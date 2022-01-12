6bb08ffe4e989ee1d5b47c005708ef2d75cfc067
jnskkmhr <junjun.k2012@gmail.com>
2022-01-12 16:56:33 +0900

    fix

--- train/tasks/semantic/modules/trainer.py
+++ train/tasks/semantic/modules/trainer.py
@@ -552,7 +552,7 @@ class Trainer():
                 output = output_mean
             else:
                 output = model(in_vol, uda=False)
-                assert output.size == proj_labels.size(). print("mismatch between output and target size...")
+                assert output.size == proj_labels.size(), print("mismatch between output and target size...")
                 loss_m = criterion(torch.log(output.clamp(min=1e-8)), proj_labels) + self.ls(output, proj_labels.long())
 
             optimizer.zero_grad()
